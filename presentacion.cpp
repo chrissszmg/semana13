302417310d81e0af79dc02e4e9f2a80ff87b5623
chrissszmg <camorales@estud.usfq.edu.ec>
2026-04-07 20:12:28 -0500

    Ejercicios de clase

---
+++ presentacion.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+
+void presentarse(){
+
+    cout << "Hola, soy Christian, el hechicero" << endl;
+  
+}
+
+using namespace std;
+int main(){
+
+
+
+presentarse();
+presentarse();
+
+
+
+    return 0;
+}
